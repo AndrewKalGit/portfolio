2b602cc0eb02a0619dc8f78fa7648ace0b9e66a1
Andrew Kal <andrewkalgit@gmail.com>
2022-09-02 03:19:43 -0400

    MVP just need to debug resume png

--- src/Resume.js
+++ src/Resume.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
-import Resumepdf from './Andrew_Kal_FrontEnd__.pdf';
+import Resumepdf2 from './AndrewKalFrontEnd.png';
 
 function Resume(props) {
 
     return (    
     <>
     <div class="flex justify-center bg-gray-900">
-        <img class="w-2/4" src={Resumepdf} alt="resume"/>
+        <img class="w-2/4" src={Resumepdf2} alt="resume"/>
     </div>
     <div class="flex justify-center bg-gray-900">
-        <a href={require("./Img/Andrew Kal-SWE-2022 Format.pdf")} download="Andrew Kal's Resume">
+        <a href={require("./Img/AndrewKalFrontEnd.pdf")} download="Andrew Kal's Resume">
         <button class="font-semi-bold w-24 h-8 bg-gray-400 hover:bg-gray-700 rounded-sm mt-4 text-white text-xs">Download</button>
         </a>
     </div>
--- src/Summary.js
+++ src/Summary.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Portrait from './andrewk.png'
+import { Link } from 'react-router-dom'
 
 function Summary(props) {
     return (
 <aside
-  class="overflow-hidden bg-gray-900 sm:grid sm:grid-cols-2 sm:items-center pt-8 sm:pt-5 md:pt-6 -mb-8"
+  class="overflow-hidden bg-gray-900 sm:grid sm:grid-cols-1 sm:items-center pt-8 sm:pt-5 md:grid-cols-1 md:items-center md:pt-5 -mb-8"
 >
   <div class="p-8 md:p-8 lg:px-16 lg:py-10">
     <div class="max-w-xl mx-auto text-center sm:text-left">
@@ -13,7 +14,7 @@ function Summary(props) {
       </h2>
 
       <p class="hidden text-gray-300 md:mt-4 md:block">
-        I'm a front-end developer with a background in sales and retail. I love solving problems, collaborating with like-minded people, and getting out of my comfort zone with hobbies like Boxing and Fitness.
+        &emsp;&emsp; I'm a front-end developer with a background in sales and retail. I love solving problems, collaborating with like-minded people, and getting out of my comfort zone with hobbies like Boxing and Fitness.
 After working to help customers find the best hardware and software for their needs, I developed a strong interest in how app development and design makes a difference in people's lives. I am currently looking for a full time position as a Front-end developer and using freelancing as a means to stay engaged in the community and hone my front-end skills. 
         </p>
         <p class="text-gray-300 md:mt-4 md:block">
@@ -21,23 +22,32 @@ After working to help customers find the best hardware and software for their ne
         <strong> Not only do I bring a strong frontend backgroud, I thrive in a culture that shares my ability to struggle empathetically and come out on top.
         </strong>
       </p>
-
-      <div class="mt-4 md:mt-8">
-        <a
-          href="https://github.com/AndrewKalGit" target="_blank"
-          class="inline-block px-12 py-3 text-sm font-medium text-white transition rounded bg-gray-400 hover:bg-gray-700" rel="noreferrer"
+      <div>
+      <div class="flex w-11/12 mt-4 md:mt-8">
+        <Link
+          to="/projects" target="_blank"
+          class="inline-block px-12 py-3 text-sm font-medium text-white transition  bg-gray-500 hover:bg-gray-700" rel="noreferrer"
         >
-          Check out my work!
+          Check out my work! [Live]
+        </Link>
+     
+         <a
+          href="https://github.com/andrewkalgit" target="_blank"
+          class="inline-block px-12 py-3 text-sm font-medium text-white transition bg-gray-400 hover:bg-gray-700" rel="noreferrer"
+        >
+          Check out my work! [Code]
         </a>
+      </div>
       </div>
     </div>
   </div>
-
+  <div class="flex justify-center">
   <img
     alt="#"
     src={Portrait}
-    class="w-11/12"
+    class="w-full sm:w-1/4"
   />
+  </div>
 </aside>
 
 
